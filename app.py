27aab7c8b7ec5e617599a243002fac9f3f1dd43f
Chenyi0309 <124005669+Chenyi0309@users.noreply.github.com>
2025-06-04 20:50:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import pyomo.environ as pyo
+import pyomo.environ as pyo 
 
 # Set up the Streamlit page
 st.set_page_config(page_title="ICU Allocation Dashboard", layout="wide")
